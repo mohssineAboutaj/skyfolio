83b72a058ec22ac21c7d5a06297c76e48d818c2a
mohssineAboutaj <mohssineaboutaj1995@gmail.com>
2024-02-17 18:20:38 +0100

    feat: setup vuetify

--- app.vue
+++ app.vue
@@ -1,5 +1,33 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <v-card flat>
+    <v-toolbar color="primary" dark extended flat>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </v-toolbar>
+
+    <v-container>
+      <v-card style="margin-top: -80px">
+        <v-toolbar flat> 
+          <v-toolbar-title> Title </v-toolbar-title>
+
+          <v-spacer></v-spacer>
+
+          <v-btn icon>
+            <v-icon>mdi-magnify</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-apps</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </v-toolbar>
+
+        <v-divider></v-divider>
+
+        <v-card-text style="height: 200px"></v-card-text>
+      </v-card>
+    </v-container>
+  </v-card>
 </template>
---
+++ vuetify.config.ts
@@ -0,0 +1,22 @@
+import { defineVuetifyConfiguration } from "vuetify-nuxt-module/custom-configuration"
+import { blue } from "vuetify/lib/util/colors.mjs"
+
+const colorPalette = {
+  primary: blue.darken1,
+}
+
+export default defineVuetifyConfiguration({
+  theme: {
+    defaultTheme: "dark",
+    themes: {
+      dark: {
+        colors: { ...colorPalette },
+        dark: true,
+      },
+      light: {
+        colors: { ...colorPalette },
+        dark: false,
+      },
+    },
+  },
+})
