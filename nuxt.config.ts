d2f2ba17382810bd3fd626057664f3aaf3198191
mohssineAboutaj <mohssineaboutaj1995@gmail.com>
2024-02-26 16:54:30 +0100

    feat: setup pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["vuetify-nuxt-module"],
+  modules: ["vuetify-nuxt-module", "@pinia/nuxt"],
   vuetify: {
     moduleOptions: {
       /* module specific options */
