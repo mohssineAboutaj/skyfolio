12b90fac0ed50bdbe1d366f56e91a1d13c46da6b
mohssineAboutaj <mohssineaboutaj1995@gmail.com>
2024-02-27 13:14:32 +0100

    feat: create header section

---
+++ components/HeaderComponent.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { useAboutInfoStore } from "@/stores/about"
+import type { Link, Social } from "@/types/general"
+
+const info = useAboutInfoStore().getInfo
+
+const user = {
+  ...info,
+  avatar: "/uploads/" + info.avatar,
+}
+</script>
+
+<template>
+  <v-parallax src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
+    <v-container class="fill-height">
+      <v-row
+        class="justify-center align-center flex-column-reverse flex-md-row"
+      >
+        <v-col cols="12" md="6" class="font-weight-thin">
+          <h1 class="text-h3 mb-8 text-capitalize">
+            Hi, I'm <b>{{ user.fullName }}</b>
+          </h1>
+          <h3 class="text-h5 mb-8 text-capitalize">
+            Am a <b>{{ user.job }}</b>
+          </h3>
+        </v-col>
+        <v-col cols="12" md="6" class="text-center">
+          <v-avatar :size="300" class="elevation-12 mx-auto mb-8">
+            <v-img :src="user.avatar" :alt="`${user.fullName} avatar`"></v-img>
+          </v-avatar>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-parallax>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,3 @@
 <template>
-  <v-container>
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
-    cupiditate, nihil, veritatis blanditiis fugit quisquam sint quo modi veniam
-    itaque molestias expedita, quidem sapiente voluptates sed ad necessitatibus
-    accusantium laborum!
-  </v-container>
+  <HeaderComponent />
 </template>
