d392ee2bfc522363243325b5af6933fe6d8d2f7b
mohssineAboutaj <mohssineaboutaj1995@gmail.com>
2024-03-03 23:24:11 +0100

    feat: make toolbar fixed

--- app.vue
+++ app.vue
@@ -76,7 +76,48 @@ $subscribe((mutate, state) => {
   <Particles />
 
   <v-app>
-    <v-toolbar color="primary">
+    <v-navigation-drawer
+      v-model="drawer"
+      class="hidden-md-and-up"
+      disable-resize-watcher
+    >
+      <div class="d-flex align-center justify-center mt-16 mb-8">
+        <v-avatar :size="80">
+          <v-img :src="user.avatar" :alt="`${user.fullName} avatar`"></v-img>
+        </v-avatar>
+      </div>
+      <h3 class="text-h5 mb-8 text-center text-capitalize">
+        {{ user.fullName }}
+      </h3>
+
+      <div class="d-flex align-center justify-space-around mb-8">
+        <v-btn
+          v-for="social in socials"
+          :key="social.icon"
+          :href="social.link"
+          target="_blank"
+          icon
+        >
+          <Icon :name="social.icon" size="24" />
+        </v-btn>
+      </div>
+
+      <v-list density="comfortable" class="pr-0" nav>
+        <v-list-item
+          v-for="link in links"
+          :key="link.value"
+          :prepend-icon="link.icon"
+          :title="link.title"
+          :value="link.value"
+          active-class="bg-primary"
+          :active="link.isCurrent"
+          @click="goToTarget(link)"
+          :to="`/#${link.value}`"
+        ></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar color="primary" class="position-fixed" style="z-index: 999">
       <v-app-bar-nav-icon v-if="showBackBtn" @click="router.go(-1)">
         <v-icon>mdi-arrow-left</v-icon>
       </v-app-bar-nav-icon>
@@ -114,43 +155,6 @@ $subscribe((mutate, state) => {
       <span>&copy; {{ new Date().getFullYear() }}</span>
       <b>{{ user.fullName }}</b>
     </v-footer>
-
-    <v-navigation-drawer v-model="drawer" class="hidden-md-and-up">
-      <div class="d-flex align-center justify-center mt-16 mb-8">
-        <v-avatar :size="80">
-          <v-img :src="user.avatar" :alt="`${user.fullName} avatar`"></v-img>
-        </v-avatar>
-      </div>
-      <h3 class="text-h5 mb-8 text-center text-capitalize">
-        {{ user.fullName }}
-      </h3>
-
-      <div class="d-flex align-center justify-space-around mb-8">
-        <v-btn
-          v-for="social in socials"
-          :key="social.icon"
-          :href="social.link"
-          target="_blank"
-          icon
-        >
-          <Icon :name="social.icon" size="24" />
-        </v-btn>
-      </div>
-
-      <v-list density="comfortable" class="pr-0" nav>
-        <v-list-item
-          v-for="link in links"
-          :key="link.value"
-          :prepend-icon="link.icon"
-          :title="link.title"
-          :value="link.value"
-          active-class="bg-primary"
-          :active="link.isCurrent"
-          @click="goToTarget(link)"
-          :to="`/#${link.value}`"
-        ></v-list-item>
-      </v-list>
-    </v-navigation-drawer>
   </v-app>
 </template>
 
