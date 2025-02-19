252d3757423d883f95dc78400c1a5afcaf0a3d69
mohssineAboutaj <mohssineaboutaj1995@gmail.com>
2025-02-19 16:58:07 +0100

    refactor: remove the contact form

--- components/ContactsComponent.vue
+++ components/ContactsComponent.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-import { object, string } from "yup"
 import type { Contact } from "~/types/general"
 
 // store
@@ -8,24 +7,6 @@ const { getAll } = useContactStore()
 // data
 /// reactive
 const contacts: Contact[] = reactive([])
-const showAlert = ref(false)
-
-// validation
-const { defineField, handleSubmit, resetForm, meta, isSubmitting } = useForm({
-  validationSchema: object({
-    email: string().email().required(),
-    subject: string().required(),
-    message: string().required(),
-  }),
-})
-const vuetifyConfig = (state: { errors: any }) => ({
-  props: {
-    "error-messages": state.errors,
-  },
-})
-const [email, emailProps] = defineField("email", vuetifyConfig)
-const [subject, subjectProps] = defineField("subject", vuetifyConfig)
-const [message, messageProps] = defineField("message", vuetifyConfig)
 
 // hooks
 onMounted(() => {
@@ -33,130 +14,42 @@ onMounted(() => {
     contacts.push(contact)
   })
 })
-
-// methods
-/// submit form
-const onSubmit = handleSubmit((values) => {
-  const $mail: any = useNuxtApp().$mail
-  $mail
-    .send({
-      from: values.email,
-      subject: values.subject,
-      text: values.message,
-    })
-    .then(() => {
-      showAlert.value = true
-      resetForm()
-    })
-})
 </script>
 
 <template>
-  <v-snackbar v-model="showAlert" color="success" location="top">
-    Email sent successfully
-
-    <template v-slot:actions>
-      <v-btn color="primary" variant="text" @click="showAlert = false">
-        <v-icon>mdi-close</v-icon>
-      </v-btn>
-    </template>
-  </v-snackbar>
-
   <v-card
     id="contacts"
     title="contacts"
     subtitle="If you have any questions or want to work together, feel free to contact me."
   >
     <v-card-text>
-      <v-row :class="{ 'flex-column-reverse': $vuetify.display.mdAndDown }">
-        <v-col cols="12" md="6">
-          <v-card-text class="text-h6">
-            <v-row v-if="contacts.length == 0" class="justify-space-around">
-              <v-col v-for="n in 3" :key="`contact-skeleton-${n}`">
-                <v-skeleton-loader type="button"></v-skeleton-loader>
-              </v-col>
-            </v-row>
+      <v-card-text class="text-h6">
+        <v-row v-if="contacts.length == 0" class="justify-space-around">
+          <v-col v-for="n in 12" :key="`contact-skeleton-${n}`">
+            <v-skeleton-loader type="avatar"></v-skeleton-loader>
+          </v-col>
+        </v-row>
 
-            <v-row v-else class="justify-space-around">
-              <v-col
-                v-for="contact in contacts"
-                :key="contact.id"
-                v-bind="
-                  $vuetify.display.mdAndDown
-                    ? { cols: 'auto' }
-                    : { cols: 12, md: 6, lg: 4 }
-                "
-              >
-                <v-btn
-                  :color="contact.color"
-                  :href="contact.link"
-                  target="_blank"
-                  :title="contact.label"
-                  :aria-label="contact.label"
-                  :class="
-                    !$vuetify.display.mdAndDown ? 'd-flex justify-start' : ''
-                  "
-                  :icon="$vuetify.display.mdAndDown"
-                  :block="!$vuetify.display.mdAndDown"
-                >
-                  <Icon :name="contact.icon" size="20" />
-                  <span class="hidden-md-and-down mx-2">
-                    {{ contact.label }}
-                  </span>
-                </v-btn>
-              </v-col>
-            </v-row>
-          </v-card-text>
-        </v-col>
-        <v-col cols="12" md="6">
-          <v-form @submit="onSubmit" validate-on="blur">
-            <v-row>
-              <v-col cols="12" md="6">
-                <v-text-field
-                  v-model="email"
-                  v-bind="emailProps"
-                  label="Email"
-                  aria-label="email"
-                  type="email"
-                  outlined
-                  prepend-icon="mdi-email"
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12" md="6">
-                <v-text-field
-                  v-model="subject"
-                  v-bind="subjectProps"
-                  label="Subject"
-                  aria-label="subject"
-                  outlined
-                  prepend-icon="mdi-format-title"
-                ></v-text-field>
-              </v-col>
-            </v-row>
-            <v-textarea
-              v-model="message"
-              v-bind="messageProps"
-              label="Message"
-              aria-label="message"
-              outlined
-              prepend-icon="mdi-message"
-            ></v-textarea>
-            <v-row class="my-4 mx-0 ga-6 justify-end">
-              <v-btn color="error" variant="outlined" @click="resetForm">
-                Reset
-              </v-btn>
-              <v-btn
-                color="primary"
-                type="submit"
-                :loading="isSubmitting"
-                :disabled="!meta.valid"
-              >
-                Submit
-              </v-btn>
-            </v-row>
-          </v-form>
-        </v-col>
-      </v-row>
+        <v-row v-else class="justify-space-around">
+          <v-col v-for="contact in contacts" :key="contact.id" cols="auto">
+            <v-btn
+              :color="contact.color"
+              :href="contact.link"
+              target="_blank"
+              :title="contact.label"
+              :aria-label="contact.label"
+              :class="[
+                'circle-btn',
+                !$vuetify.display.mdAndDown ? 'd-flex justify-center' : '',
+              ]"
+              :icon="true"
+              :block="false"
+            >
+              <Icon :name="contact.icon" size="20" />
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-card-text>
     </v-card-text>
   </v-card>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,29 +3,31 @@ import aboutInfo from "./data/about/info.data"
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   modules: [
     "@nuxtjs/seo",
     "vuetify-nuxt-module",
     "@pinia/nuxt",
     "nuxt-swiper",
     "nuxt-icon",
-    "@vee-validate/nuxt",
     "nuxt-particles",
     "@nuxtjs/google-fonts",
-    "nuxt-mail",
     "nuxt-gtag",
   ],
+
   /// extend the nuxt modules
   googleFonts: {
     families: { Quicksand: true, Charm: true },
     display: "swap", // 'auto' | 'block' | 'swap' | 'fallback' | 'optional'
   },
+
   vuetify: {
     moduleOptions: {
       /* module specific options */
     },
     vuetifyOptions: "./vuetify.config.ts",
   },
+
   swiper: {
     // Swiper options
     //----------------------
@@ -33,17 +35,7 @@ export default defineNuxtConfig({
     // styleLang: 'css',
     // modules: ['navigation', 'pagination'], // all modules are imported by default
   },
-  mail: {
-    message: { to: process.env.SMTP_USERNAME },
-    smtp: {
-      host: "smtp.gmail.com",
-      port: 587,
-      auth: {
-        user: process.env.SMTP_USERNAME,
-        pass: process.env.SMTP_PASSWORD,
-      },
-    },
-  },
+
   site: {
     url: "https://mohssineaboutaj.com/",
     name: `${aboutInfo.fullName} Portfolio`,
@@ -57,7 +49,10 @@ export default defineNuxtConfig({
       ...aboutInfo.jobs,
     ],
   },
+
   gtag: {
     id: "G-KRLF8VEECK",
   },
+
+  compatibilityDate: "2025-02-19",
 })
